cbf5e3a3f28ff6ab33f6e8ed45f7be825666329d
yours-noir <janteduhadam@gmail.com>
2026-09-19 12:23:54 +0700

    Refactor onload function to start function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
+function start() {
+  document.body.classList.remove("not-loaded");
+}
